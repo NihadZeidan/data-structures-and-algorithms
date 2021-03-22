f6a629ed3ecfbb3684a86042a24123956c975b6c
NihadZeidan <nihadzeidan@gmail.com>
2021-03-22 21:21:05 +0200

    solve challenge 02

--- code-challenges/challenges-02.test.js
+++ code-challenges/challenges-02.test.js
@@ -23,7 +23,11 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 ------------------------------------------------------------------------------------------------ */
 
 const appendTheEnd = (str) => {
-  // Solution code here...
+  let newString = str
+
+  newString += " The end." ;
+
+  return newString
 };
 
 /* ------------------------------------------------------------------------------------------------
